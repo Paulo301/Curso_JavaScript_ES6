008fb42edf456c9e502249b2f405ec087fb774b9
Paulo301 <paulovictor1918@gmail.com>
2020-08-03 02:51:28 -0300

    Módulo 1: Conceitos, Aula 6: Operações em arrays

--- bundle.js
+++ bundle.js
@@ -1,21 +1,41 @@
 "use strict";
 
+//Aula 5: Const & Let
 // const a = 1; //Constante
 // //a=3; //Não pode ser realizado
 // const usuario = {
 //   nome: "Paulo"
 // }
 // usuario.nome = "Victor"; //Realização de mutação
-function teste(x) {
-  //Cada conjunto de chaves é um escopo
-  var y = 2; //Variável de escopo
-
-  if (x > 5) {
-    var _y = 4;
-    console.log(x, _y);
-  }
-
-  console.log(y);
-}
-
-teste(10);
+// function teste(x) { //Cada conjunto de chaves é um escopo
+//   let y = 2; //Variável de escopo
+//   if (x > 5) {
+//     let y = 4;
+//     console.log(x, y);
+//   }
+//   console.log(y);
+// }
+// teste(10);
+//Aula 6: Operações em arrays
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var newArr = arr.map(function (item, index) {
+  //Percorre todos os itens da lista e os altera se desejado
+  return item * 2 + index;
+});
+console.log("Original ", arr);
+console.log("Map ", newArr);
+var sum = arr.reduce(function (total, next) {
+  //Tem como resultado apenas um valor, atribui o return ao total 
+  return total + next;
+});
+console.log("Reduce ", sum);
+var filter = arr.filter(function (item) {
+  //Filtra a lista mantendo apenas os casos com resultado true
+  return item % 2 == 0;
+});
+console.log("Filter: ", filter);
+var find = arr.find(function (item) {
+  //retorna o item se for encontrado ou undefined se não
+  return item === 10;
+});
+console.log("Find: ", find);
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//Aula 5: Const & Let
+
 // const a = 1; //Constante
 // //a=3; //Não pode ser realizado
 
@@ -7,15 +9,44 @@
 
 // usuario.nome = "Victor"; //Realização de mutação
 
-function teste(x) { //Cada conjunto de chaves é um escopo
-  let y = 2; //Variável de escopo
+// function teste(x) { //Cada conjunto de chaves é um escopo
+//   let y = 2; //Variável de escopo
 
-  if (x > 5) {
-    let y = 4;
+//   if (x > 5) {
+//     let y = 4;
 
-    console.log(x, y);
-  }
-  console.log(y);
-}
+//     console.log(x, y);
+//   }
+//   console.log(y);
+// }
 
-teste(10);
+// teste(10);
+
+//Aula 6: Operações em arrays
+
+const arr = [1,2,3,4,5,6,7,8,9];
+
+const newArr = arr.map(function(item, index){//Percorre todos os itens da lista e os altera se desejado
+  return item*2 + index;
+});
+
+console.log("Original ",arr);
+console.log("Map ",newArr);
+
+const sum = arr.reduce(function(total, next){//Tem como resultado apenas um valor, atribui o return ao total 
+  return total+next;
+});
+
+console.log("Reduce ",sum);
+
+const filter = arr.filter(function(item){//Filtra a lista mantendo apenas os casos com resultado true
+  return item%2==0;
+});
+
+console.log("Filter: ",filter);
+
+const find = arr.find(function(item){//retorna o item se for encontrado ou undefined se não
+  return item ===10;
+})
+
+console.log("Find: ",find);
