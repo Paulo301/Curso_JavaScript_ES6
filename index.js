df9c2ee6a0e880c17d39b4989e6a9f3e1f80f65b
Paulo301 <paulovictor1918@gmail.com>
2020-08-04 17:14:27 -0300

    Módulo 2: Webpack Server, Aula 3: Webpack dev server

--- index.js
+++
@@ -1,9 +0,0 @@
-// import somaFunction, { sub } from "./funcoes";
-// import somaFunction from "./soma";
-import * as func from "./funcoes";
-
-console.log(func.soma(1,2));
-
-console.log(func.sub(4,2));
-
-console.log(func);
