73eedefe397be82087f522d83ca72e417ac69329
Paulo301 <paulovictor1918@gmail.com>
2020-08-05 01:37:18 -0300

    Módulo 3: Async/await, Aula 1: Async/await

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
-import ClasseUsuario, {idade as IdadeUsuario} from "./functions";
+const minhaPromise = () => new Promise((resolve, reject) => {
+  setTimeout(() => {resolve("OK")}, 2000);
+});
 
-ClasseUsuario.info();
-console.log(IdadeUsuario);
+// minhaPromise()
+//   .then(response => {
+//     console.log(response);
+//   })
+//   .catch(err => {
+
+//   });
+
+// async function executaPromise(){
+//   console.log(await minhaPromise());//await so pode ser usado dentro de funções assincronas
+//   console.log(await minhaPromise());
+//   console.log(await minhaPromise());
+// }
+
+const executaPromise = async () => {
+  console.log(await minhaPromise());
+  console.log(await minhaPromise());
+  console.log(await minhaPromise());
+}
+
+executaPromise();//Se torna uma promise, da pra usar .then nela
